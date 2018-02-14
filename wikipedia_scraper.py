fcfa93d95487e6758efe2ed05acc98fee7bded5c
Ephraim Kunz <ephraimkunz@me.com>
2018-02-13 19:39:13 -0700

    Init

---
+++ wikipedia_scraper.py
@@ -0,0 +1,89 @@
+from __future__ import print_function
+import wikipedia
+import json
+import os
+import time
+
+directory = "./articles"
+
+# Skipped pages because of errors
+skipped = []
+
+# Array of dicts to serialize to json
+articles = []
+
+# Get controversial articles
+def get_controversial_list():
+    print("Getting list of controversial articles...")
+    controversial = wikipedia.page("Wikipedia:List of controversial issues")
+    links = controversial.links
+    print("Found %d controversial articles" % len(links))
+    return links
+
+def get_articles_in_list(names, callback):
+    fetched = 0
+    for link in names:
+        page = None
+        try:
+            page = wikipedia.page(link, auto_suggest=False)
+        except Exception as e:
+            skipped.append(link)
+            print("Error fetching %s, skipping" % link)
+            continue
+            
+        callback(page, fetched)
+
+        if fetched % 20 == 0:
+            print("Fetched %d of %d articles: %f%%" % (fetched, len(names), (float(fetched) / len(names) * 100)))
+
+        fetched += 1
+
+def dump_data_to_file(count):
+    structure = {}
+    structure["skipped"] = skipped
+    structure["controversial"] = articles
+
+    filename = "controversial_" + str(count) + ".json"
+
+    print("Dumping %d articles and %d skipped to file %s..." % (len(articles), len(skipped), filename))
+
+    with open(directory + '/' + filename, "w+") as f:
+        json.dump(structure, f, indent=4, sort_keys=True)
+
+    # Clear data
+    del skipped[:]
+    del articles[:]
+
+def handle_article(article, count):
+    dic = {}
+    dic["title"] = article.title
+    dic["pageid"] = article.pageid
+    dic["url"] = article.url
+    dic["content"] = article.content
+    dic["categories"] = article.categories
+    dic["image_urls"] = article.images
+    dic["links"] = article.links
+    dic["references"] = article.references
+    articles.append(dic)
+
+    if count > 0 and count % 100 == 0:
+        dump_data_to_file(count)
+
+if __name__ == "__main__":
+    # Set up scraper responsibly
+    start = time.time()
+    wikipedia.set_rate_limiting(True)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    cont = get_controversial_list()
+    get_articles_in_list(cont, handle_article)
+
+    # Dump anything left to a file
+    dump_data_to_file("last")
+
+    end = time.time()
+
+    print("Scrape took %d seconds" % end - start)
+
+    print("Done")
