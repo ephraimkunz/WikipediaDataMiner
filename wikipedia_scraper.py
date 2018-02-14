ce0a6b0b585a33b59fec489bad7548e02296528b
Ephraim Kunz <ephraimkunz@me.com>
2018-02-13 21:50:03 -0700

    Add support for fetching normal articles

--- wikipedia_scraper.py
+++ wikipedia_scraper.py
@@ -38,12 +38,12 @@ def get_articles_in_list(names, callback):
 
         fetched += 1
 
-def dump_data_to_file(count):
+def dump_data_to_file(file_prefix, count):
     structure = {}
     structure["skipped"] = skipped
     structure["controversial"] = articles
 
-    filename = "controversial_" + str(count) + ".json"
+    filename = file_prefix + "_" + str(count) + ".json"
 
     print("Dumping %d articles and %d skipped to file %s..." % (len(articles), len(skipped), filename))
 
@@ -54,24 +54,38 @@ def dump_data_to_file(count):
     del skipped[:]
     del articles[:]
 
-def handle_article(article, count):
-    dic = {}
-    try:
-        dic["title"] = article.title
-        dic["pageid"] = article.pageid
-        dic["url"] = article.url
-        dic["content"] = article.content
-        dic["categories"] = article.categories
-        dic["image_urls"] = article.images
-        dic["links"] = article.links
-        dic["references"] = article.references
-        articles.append(dic)
-    except Exception as e:
-        print("Failure to parse %s" % article.title)
-        skipped.append(article.title)
+def handle_article(file_prefix):
+    def inner_handle_article(article, count):
+        dic = {}
+        try:
+            dic["title"] = article.title
+            dic["pageid"] = article.pageid
+            dic["url"] = article.url
+            dic["content"] = article.content
+            dic["categories"] = article.categories
+            dic["image_urls"] = article.images
+            dic["links"] = article.links
+            dic["references"] = article.references
+            articles.append(dic)
+        except Exception as e:
+            print("Failure to parse %s" % article.title)
+            skipped.append(article.title)
 
-    if count > 0 and count % 100 == 0:
-        dump_data_to_file(count)
+        if count > 0 and count % 100 == 0:
+            dump_data_to_file(file_prefix, count)
+
+    return inner_handle_article
+
+# Get a random list of wikipedia articles about count big, avoiding articles found in avoid
+def get_random_list(count, avoid):
+    result = []
+
+    print("Building list of random pages...")
+    while len(result) < count:
+        pages = wikipedia.random(10)
+        pages = [page for page in pages if page not in avoid and page not in result]
+        result.extend(pages)
+    return result
 
 if __name__ == "__main__":
     # Set up scraper responsibly
@@ -80,14 +94,20 @@ if __name__ == "__main__":
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+    # Get controversial articles
     cont = get_controversial_list()
-    get_articles_in_list(cont, handle_article)
+    #get_articles_in_list(cont, handle_article("controversial"))
 
     # Dump anything left to a file
-    dump_data_to_file("last")
+    #dump_data_to_file("controversial", "last")
+
+
+
+    # Get non-controversial articles
+    normal = get_random_list(len(cont), cont)
+    get_articles_in_list(normal, handle_article("normal"))
+    dump_data_to_file("controversial", "last")
 
     end = time.time()
-
     print("Scrape took %f seconds" % (end - start))
-
     print("Done")
