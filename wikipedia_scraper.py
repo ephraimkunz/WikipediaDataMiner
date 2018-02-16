e1fe6e90e2654b181f0e37c149b06bb80891666e
Ephraim Kunz <ephraimkunz@me.com>
2018-02-15 17:28:51 -0700

    Add support for revisions, inbound links

--- wikipedia_scraper.py
+++ wikipedia_scraper.py
@@ -3,6 +3,7 @@ import wikipedia
 import json
 import os
 import time
+import requests
 
 directory = "./articles"
 
@@ -12,7 +13,7 @@ skipped = []
 # Array of dicts to serialize to json
 articles = []
 
-# Get controversial articles
+# Get array of controversial article names
 def get_controversial_list():
     print("Getting list of controversial articles...")
     controversial = wikipedia.page("Wikipedia:List of controversial issues")
@@ -20,6 +21,7 @@ def get_controversial_list():
     print("Found %d controversial articles" % len(links))
     return links
 
+# Given a list of article names and a callback, lookup each name and apply the callback to it
 def get_articles_in_list(names, callback):
     fetched = 0
     for link in names:
@@ -34,29 +36,101 @@ def get_articles_in_list(names, callback):
                 print("Error fetching %s: %s, skipping" % (link, e))
                 continue
             
-        callback(page, fetched)
+        success = callback(page, fetched)
+        if not success:
+            continue
 
-        if fetched % 20 == 0:
+        if fetched % 20 == 0 and fetched != 0:
             print("Fetched %d of %d articles: %f%%" % (fetched, len(names), (float(fetched) / len(names) * 100)))
 
         fetched += 1
 
+# Create a file with the file_prefix and count and dump json into it
 def dump_data_to_file(file_prefix, count):
     structure = {}
-    structure["skipped"] = skipped
-    structure["controversial"] = articles
+    structure["articles"] = articles
 
     filename = file_prefix + "_" + str(count) + ".json"
 
-    print("Dumping %d articles and %d skipped to file %s..." % (len(articles), len(skipped), filename))
+    print("Dumping %d articles to file %s..." % (len(articles), filename))
 
     with open(directory + '/' + filename, "w+") as f:
         json.dump(structure, f, indent=4, sort_keys=True)
 
     # Clear data
-    del skipped[:]
     del articles[:]
 
+# Return continuation if exists, else False. Continue param is specific to what type of query we are doing.
+def get_continuation(data, continue_param):
+    if 'continue' in data:
+        continuation = data['continue'][continue_param]
+        assert(continuation and 'batchcomplete' not in data)
+        return continuation
+    
+    assert 'batchcomplete' in data
+    return False
+
+def get_inbound(data, pageid):
+    linkshere = data['query']['pages'][pageid]['linkshere']
+    return list(map(lambda x: x['title'], linkshere))
+
+def get_edits(data, pageid):
+    edits = data['query']['pages'][pageid]['revisions']
+    return list(map(lambda x: x['timestamp'], edits))
+
+# Returns array of revision timestamps of this page
+def get_revisions(pageid):
+    results = []
+    raw_url = "https://en.wikipedia.org//w/api.php?action=query&format=json&prop=revisions&pageids={}&rvprop=timestamp&rvlimit=max"
+    continue_param = 'rvcontinue'
+
+    first_page = raw_url.format(pageid)
+    resp = requests.get(first_page)
+    data = json.loads(resp.text)
+
+    results.extend(get_edits(data, pageid))
+
+    continuation = get_continuation(data, continue_param)
+    while continuation:
+        next_page = raw_url + '&' + continue_param + '={}'
+        next_page = next_page.format(pageid, continuation)
+
+        resp = requests.get(next_page)
+        data = json.loads(resp.text)
+        results.extend(get_edits(data, pageid))
+        continuation = get_continuation(data, continue_param)
+
+    results.sort()
+    return results
+
+# Returns array of page titles inbound to this page
+def get_inbound_links(pageid):
+    results = []
+    raw_url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=linkshere&pageids={}&lhprop=title&lhnamespace=0&lhlimit=max"
+    continue_param = 'lhcontinue'
+    
+    first_page = raw_url.format(pageid)
+    resp = requests.get(first_page)
+    data = json.loads(resp.text)
+
+    results.extend(get_inbound(data, pageid))
+
+    continuation = get_continuation(data, continue_param)
+    while continuation:
+        next_page = raw_url + '&' + continue_param + '={}'
+        next_page = next_page.format(pageid, continuation)
+
+        resp = requests.get(next_page)
+        data = json.loads(resp.text)
+        results.extend(get_inbound(data, pageid))
+        continuation = get_continuation(data, continue_param)
+
+    results.sort()
+    return results
+
+
+
+# Callback for get_articles_in_list
 def handle_article(file_prefix):
     def inner_handle_article(article, count):
         dic = {}
@@ -69,13 +143,18 @@ def handle_article(file_prefix):
             dic["image_urls"] = article.images
             dic["links"] = article.links
             dic["references"] = article.references
+            dic['inbound_links'] = get_inbound_links(article.pageid)
+            dic['revisions'] = get_revisions(article.pageid)
             articles.append(dic)
         except Exception as e:
-            print("Failure to parse %s" % article.title)
+            print("Failure to parse %s: %s" % (article.title, e))
             skipped.append(article.title)
+            return False
 
         if count > 0 and count % 100 == 0:
             dump_data_to_file(file_prefix, count)
+        
+        return True
 
     return inner_handle_article
 
@@ -90,6 +169,21 @@ def get_random_list(count, avoid):
         result.extend(pages)
     return result
 
+# Dumps list of skipped articles to file
+def dump_skipped_to_file(postfix):
+    structure = {}
+    structure["skipped"] = skipped
+
+    filename = "skipped_" + postfix + ".json"
+
+    print("Dumping %d skipped names to file %s..." % (len(skipped), filename))
+
+    with open(directory + '/' + filename, "w+") as f:
+            json.dump(structure, f, indent=4, sort_keys=True)
+
+    # Clear data
+    del articles[:]
+
 if __name__ == "__main__":
     # Set up scraper responsibly
     start = time.time()
@@ -99,17 +193,17 @@ if __name__ == "__main__":
 
     # Get controversial articles
     cont = get_controversial_list()
-    #get_articles_in_list(cont, handle_article("controversial"))
+    get_articles_in_list(cont[:20], handle_article("controversial"))
 
     # Dump anything left to a file
-    #dump_data_to_file("controversial", "last")
-
-
+    dump_data_to_file("controversial", "last")
 
     # Get non-controversial articles
-    normal = get_random_list(len(cont), cont)
+    normal = get_random_list(20, cont)
     get_articles_in_list(normal, handle_article("normal"))
     dump_data_to_file("normal", "last")
+
+    dump_skipped_to_file("all")
 
     end = time.time()
     print("Scrape took %f seconds" % (end - start))
