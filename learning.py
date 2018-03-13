264777c372629e7e44ab2960d82db130a3f813aa
Ephraim Kunz <ephraimkunz@me.com>
2018-03-13 17:56:32 -0600

    Add ensemble methods

--- learning.py
+++ learning.py
@@ -4,7 +4,7 @@ import sklearn.linear_model
 from sklearn import preprocessing
 from sklearn.model_selection import cross_val_score
 
-
+# Data preparation
 dataset = arff.load(open('./combined_binned_norev.arff', 'rb'))
 data = np.array(dataset['data'])
 
@@ -12,30 +12,55 @@ data = sklearn.utils.shuffle(data)
 
 num_attr = len(data[0]) - 1
 attr = data[:,:num_attr].astype(float)
+attr = preprocessing.normalize(attr) # Normalize helps kNN, MLP, seems to hurt perceptron
 labels = data[:,num_attr]
 le = preprocessing.LabelEncoder()
 le.fit(labels)
 newlabels = le.transform(labels)
 
 # Sklearn Perceptron
-model = sklearn.linear_model.Perceptron()
-scores = cross_val_score(model, attr, newlabels, cv=10)
+m1 = sklearn.linear_model.Perceptron()
+scores = cross_val_score(m1, attr, newlabels, cv=10)
 print("Perceptron: {}".format(scores.mean()))
 
 # Sklearn kNN
 import sklearn.neighbors
-model = sklearn.neighbors.KNeighborsClassifier(n_neighbors=10, weights='distance')
-scores = cross_val_score(model, attr, newlabels, cv=10)
+m2 = sklearn.neighbors.KNeighborsClassifier(n_neighbors=45, weights='distance')
+scores = cross_val_score(m2, attr, newlabels, cv=10)
 print("kNN: {}".format(scores.mean()))
 
 # Sklearn MLP
 import sklearn.neural_network
-model = sklearn.neural_network.MLPClassifier()
-scores = cross_val_score(model, attr, newlabels, cv=10)
+m3 = sklearn.neural_network.MLPClassifier(solver='lbfgs', activation='logistic')
+scores = cross_val_score(m3, attr, newlabels, cv=10)
 print("MLP: {}".format(scores.mean()))
 
 # Sklearn DT
 import sklearn.tree
-model = sklearn.tree.DecisionTreeClassifier()
-scores = cross_val_score(model, attr, newlabels, cv=10)
+m4 = sklearn.tree.DecisionTreeClassifier()
+scores = cross_val_score(m4, attr, newlabels, cv=10)
 print("DT: {}".format(scores.mean()))
+
+#### Ensembles ####
+
+# Ada boost
+import sklearn.ensemble
+model = sklearn.ensemble.AdaBoostClassifier()
+scores = cross_val_score(model, attr, newlabels, cv=10)
+print("ADA-Boost: {}".format(scores.mean()))
+
+# Bagging
+model = sklearn.ensemble.BaggingClassifier(n_jobs=-1, n_estimators=15)
+scores = cross_val_score(model, attr, newlabels, cv=20)
+print("Bagging: {}".format(scores.mean()))
+
+# Extra trees
+model = sklearn.ensemble.ExtraTreesClassifier(n_estimators=10, n_jobs=-1)
+scores = cross_val_score(model, attr, newlabels, cv=20)
+print("Extra trees: {}".format(scores.mean()))
+
+# Extra trees
+model = sklearn.ensemble.RandomForestClassifier(n_jobs=-1)
+scores = cross_val_score(model, attr, newlabels, cv=10)
+print("Random Forest: {}".format(scores.mean()))
+
