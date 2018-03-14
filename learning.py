c0022a1b02553af8bb0cda3de268fc3c78bf70c4
Ephraim Kunz <ephraimkunz@me.com>
2018-03-13 18:14:15 -0600

    Better parallelization

--- learning.py
+++ learning.py
@@ -20,25 +20,25 @@ newlabels = le.transform(labels)
 
 # Sklearn Perceptron
 m1 = sklearn.linear_model.Perceptron()
-scores = cross_val_score(m1, attr, newlabels, cv=10)
+scores = cross_val_score(m1, attr, newlabels, cv=10, n_jobs=-1)
 print("Perceptron: {}".format(scores.mean()))
 
 # Sklearn kNN
 import sklearn.neighbors
 m2 = sklearn.neighbors.KNeighborsClassifier(n_neighbors=45, weights='distance')
-scores = cross_val_score(m2, attr, newlabels, cv=10)
+scores = cross_val_score(m2, attr, newlabels, cv=10, n_jobs=-1)
 print("kNN: {}".format(scores.mean()))
 
 # Sklearn MLP
 import sklearn.neural_network
 m3 = sklearn.neural_network.MLPClassifier(solver='lbfgs', activation='logistic')
-scores = cross_val_score(m3, attr, newlabels, cv=10)
+scores = cross_val_score(m3, attr, newlabels, cv=10, n_jobs=-1)
 print("MLP: {}".format(scores.mean()))
 
 # Sklearn DT
 import sklearn.tree
 m4 = sklearn.tree.DecisionTreeClassifier()
-scores = cross_val_score(m4, attr, newlabels, cv=10)
+scores = cross_val_score(m4, attr, newlabels, cv=10, n_jobs=-1)
 print("DT: {}".format(scores.mean()))
 
 #### Ensembles ####
@@ -46,21 +46,21 @@ print("DT: {}".format(scores.mean()))
 # Ada boost
 import sklearn.ensemble
 model = sklearn.ensemble.AdaBoostClassifier()
-scores = cross_val_score(model, attr, newlabels, cv=10)
+scores = cross_val_score(model, attr, newlabels, cv=10, n_jobs=-1)
 print("ADA-Boost: {}".format(scores.mean()))
 
 # Bagging
-model = sklearn.ensemble.BaggingClassifier(n_jobs=-1, n_estimators=15)
-scores = cross_val_score(model, attr, newlabels, cv=20)
+model = sklearn.ensemble.BaggingClassifier(n_estimators=15)
+scores = cross_val_score(model, attr, newlabels, cv=20, n_jobs=-1)
 print("Bagging: {}".format(scores.mean()))
 
 # Extra trees
-model = sklearn.ensemble.ExtraTreesClassifier(n_estimators=10, n_jobs=-1)
-scores = cross_val_score(model, attr, newlabels, cv=20)
+model = sklearn.ensemble.ExtraTreesClassifier(n_estimators=10)
+scores = cross_val_score(model, attr, newlabels, cv=20, n_jobs=-1)
 print("Extra trees: {}".format(scores.mean()))
 
 # Random forest
-model = sklearn.ensemble.RandomForestClassifier(n_jobs=-1)
-scores = cross_val_score(model, attr, newlabels, cv=10)
+model = sklearn.ensemble.RandomForestClassifier()
+scores = cross_val_score(model, attr, newlabels, cv=10, n_jobs=-1)
 print("Random Forest: {}".format(scores.mean()))
 
